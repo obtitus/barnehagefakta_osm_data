c8fe319acec222e6ea06d45db11f00de429305bd
Øystein Bjørndal <obtitus@gmail.com>
2020-12-22 12:39:13 +0100

    data update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# clone in parent directory
+#git clone https://github.com/obtitus/barnehagefakta_osm
+pip install -r ../requirements.txt
+
+# yeah, not very pythonic I know...
+git clone https://github.com/obtitus/py_import_utility_to_osm
+mv py_import_utility_to_osm ../utility_to_osm
+cd ../utility_to_osm
+git clone https://github.com/xificurk/osmapis
+cp osmapis/osmapis.py .
